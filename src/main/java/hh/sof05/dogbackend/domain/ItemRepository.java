c0f4cedbee61951d7e4943a4aa1f7a1a9a4e74cd
pr0fix <bgx152@myy.haaga-helia.fi>
2023-10-30 12:38:20 +0200

    Added repositories for entities

---
+++ src/main/java/hh/sof05/dogbackend/domain/ItemRepository.java
@@ -0,0 +1,7 @@
+package hh.sof05.dogbackend.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemRepository extends CrudRepository<Item, Long> {
+    
+}
--- src/main/java/hh/sof05/dogbackend/domain/User.java
+++ src/main/java/hh/sof05/dogbackend/domain/User.java
@@ -1,3 +1,5 @@
+package hh.sof05.dogbackend.domain;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -71,6 +73,4 @@ public class User {
         this.role = role;
     }
 
-    
-    
 }
