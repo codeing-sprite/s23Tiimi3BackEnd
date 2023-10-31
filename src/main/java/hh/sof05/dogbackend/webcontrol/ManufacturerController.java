bd5fdaf9a5b0d29ff471b5be47c89373c31e2032
pr0fix <bgx152@myy.haaga-helia.fi>
2023-10-31 13:08:29 +0200

    Made adding manufacturers possible

--- src/main/java/hh/sof05/dogbackend/webcontrol/ManufacturerController.java
+++ src/main/java/hh/sof05/dogbackend/webcontrol/ManufacturerController.java
@@ -1,8 +1,30 @@
 package hh.sof05.dogbackend.webcontrol;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hh.sof05.dogbackend.domain.Manufacturer;
+import hh.sof05.dogbackend.domain.ManufacturerRepository;
 
 @Controller
 public class ManufacturerController {
-    
+
+    @Autowired
+    ManufacturerRepository manufacturerRepository;
+
+    @GetMapping("/addmanufacturer")
+    public String addManufacturer(Model model) {
+        model.addAttribute("manufacturer", new Manufacturer());
+        return ("/addmanufacturer");
+    }
+
+    @PostMapping("/savemanufacturer")
+    public String saveManufacturer(Manufacturer manufacturer) {
+        manufacturerRepository.save(manufacturer);
+        return ("redirect:items");
+    }
+
 }
