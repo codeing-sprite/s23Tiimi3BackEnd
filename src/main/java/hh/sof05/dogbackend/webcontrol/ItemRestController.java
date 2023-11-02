3a7133599a3d32234c512ba3ddf4fdadbc260cd4
pr0fix <bgx152@myy.haaga-helia.fi>
2023-11-02 13:00:23 +0200

    Added @CrossOrigin

--- src/main/java/hh/sof05/dogbackend/webcontrol/ItemRestController.java
+++ src/main/java/hh/sof05/dogbackend/webcontrol/ItemRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import hh.sof05.dogbackend.domain.Item;
 import hh.sof05.dogbackend.domain.ItemRepository;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class ItemRestController {
-    
+
     @Autowired
     private ItemRepository itemRepository;
 
-     @GetMapping("/restitems")
-     List<Item> listAllItems() {
-         return (List<Item>) itemRepository.findAll();
-     }
+    @GetMapping("/restitems")
+    List<Item> listAllItems() {
+        return (List<Item>) itemRepository.findAll();
+    }
 
-     @GetMapping("/restitems/{id}")
-     Optional<Item> getItemById(@PathVariable("id") long itemId) {
+    @GetMapping("/restitems/{id}")
+    Optional<Item> getItemById(@PathVariable("id") long itemId) {
         return itemRepository.findById(itemId);
-     } 
-
+    }
 
 }
