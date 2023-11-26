cd668a24c3f21a0b7e0ad162adc0272d409ef544
pr0fix <bgx152@myy.haaga-helia.fi>
2023-11-26 15:36:18 +0200

    Cors filter

--- src/main/java/hh/sof05/dogbackend/WebSecurityConfig.java
+++ src/main/java/hh/sof05/dogbackend/WebSecurityConfig.java
@@ -9,52 +9,65 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
-    @Autowired
-    private UserDetailServiceImpl userDetailServiceImpl;
+        @Autowired
+        private UserDetailServiceImpl userDetailServiceImpl;
 
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(antMatcher("/")).permitAll()
-                        .requestMatchers(antMatcher("/login")).permitAll()
-                        .requestMatchers(antMatcher("/css/**")).permitAll()
-                        .requestMatchers(antMatcher("/h2-console/**")).permitAll()
-                        .requestMatchers(antMatcher("/rest/**")).permitAll()
-                        .anyRequest().authenticated())
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(toH2Console()))
-                .headers(headers -> headers
-                        .frameOptions(frameoptions -> frameoptions
-                                .disable()))
-                .formLogin(formlogin -> formlogin
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/itemlist", true)
-                        .permitAll()
-                        )
-                .logout(logout -> logout
-                        .permitAll())
-                .exceptionHandling(exception -> exception
-                        .authenticationEntryPoint(unauthorizedEntryPoint()));
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+                http
+                                .cors(cors -> {
+                                        CorsConfiguration configuration = new CorsConfiguration();
+                                        configuration.setAllowedOrigins(Arrays.asList("*"));
+                                        configuration.setAllowedMethods(Arrays.asList("*"));
+                                        configuration.setAllowedHeaders(List.of("*"));
+                                        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                                        source.registerCorsConfiguration("/**", configuration);
+                                        cors.configurationSource(request -> configuration);
+                                })
+                                .authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers(antMatcher("/")).permitAll()
+                                                .requestMatchers(antMatcher("/login")).permitAll()
+                                                .requestMatchers(antMatcher("/css/**")).permitAll()
+                                                .requestMatchers(antMatcher("/h2-console/**")).permitAll()
+                                                .requestMatchers(antMatcher("/rest/**")).permitAll()
+                                                .anyRequest().authenticated())
+                                .csrf(csrf -> csrf
+                                                .ignoringRequestMatchers(toH2Console()))
+                                .headers(headers -> headers
+                                                .frameOptions(frameoptions -> frameoptions
+                                                                .disable()))
+                                .formLogin(formlogin -> formlogin
+                                                .loginPage("/login")
+                                                .defaultSuccessUrl("/itemlist", true)
+                                                .permitAll())
+                                .logout(logout -> logout
+                                                .permitAll())
+                                .exceptionHandling(exception -> exception
+                                                .authenticationEntryPoint(unauthorizedEntryPoint()));
+                return http.build();
+        }
 
-    @Bean
-    public AuthenticationEntryPoint unauthorizedEntryPoint() {
-        return new CustomAuthenticationEntryPoint("/");
-    }
+        @Bean
+        public AuthenticationEntryPoint unauthorizedEntryPoint() {
+                return new CustomAuthenticationEntryPoint("/");
+        }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailServiceImpl).passwordEncoder(new BCryptPasswordEncoder());
-    }
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+                auth.userDetailsService(userDetailServiceImpl).passwordEncoder(new BCryptPasswordEncoder());
+        }
 
 }
